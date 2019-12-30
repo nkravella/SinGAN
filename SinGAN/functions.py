6ab8360beea1fdcfd59bfbcadd26aab89775e2d9
Derrick Schultz <dvsmethid@gmail.com>
2019-12-30 15:50:52 -0500

    fix num_samples for animation

--- SinGAN/functions.py
+++ SinGAN/functions.py
@@ -290,7 +290,10 @@ def post_config(opt):
     opt.out_ = 'TrainedModels/%s/scale_factor=%f/' % (opt.input_name[:-4], opt.scale_factor)
     if opt.mode == 'SR':
         opt.alpha = 100
-    opt.num_samples = opt.num_samples
+    if opt.mode == 'animation':
+        opt.num_samples = 1
+    elif:
+        opt.num_samples = opt.num_samples
 
     if opt.manualSeed is None:
         opt.manualSeed = random.randint(1, 10000)
