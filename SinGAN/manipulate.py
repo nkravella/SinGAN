72bd9bfff0d19d8c299fab9d96093bbb37154de1
Derrick Schultz <dvsmethid@gmail.com>
2019-12-31 18:58:00 +0000

    temp frames

--- SinGAN/manipulate.py
+++ SinGAN/manipulate.py
@@ -150,7 +150,7 @@ def generate_video(Gs,Zs,reals,NoiseAmp,opt,alpha=0.1,beta=0.9,start_scale=2,fps
 
             images_cur.append(I_curr)
             img = Image.fromarray(np.asarray(I_curr.cpu()), 'RGB')
-            img.save('%s/start_scale=%d/alpha=%f_beta=%f_%04d.png' % (dir2save,start_scale,alpha,beta,count))
+            img.save('%s/start_scale=%d/alpha=%f_beta=%f_frames/_%04d.png' % (dir2save,start_scale,alpha,beta,count))
         count += 1
     del images_cur
 
